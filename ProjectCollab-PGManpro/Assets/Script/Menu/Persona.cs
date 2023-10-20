5921e415e60558e30150b4246c3fd7ac7829820f
david <davidvincentius@gmail.com>
2023-10-20 12:06:30 +0700

    tempMenu

--- ProjectCollab-PGManpro/Assets/Script/Menu/Persona.cs
+++ ProjectCollab-PGManpro/Assets/Script/Menu/Persona.cs
@@ -6,18 +6,30 @@ using UnityEngine.UI;
 
 public class Persona : MonoBehaviour
 {
-
-    public TMP_Text score;
     public Slider slide;
+    public TMP_Text type;
+    int pointType;
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (string.Equals(type.text.ToString(), "Passive")){
+            pointType = 1;
+        } else if (string.Equals(type.text.ToString(), "Shy")) {
+            pointType = 2;
+        } else if (string.Equals(type.text.ToString(), "Nerd")) {
+            pointType = 3;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        score.text=Mathf.Floor(slide.value*100).ToString();
+        if (pointType==1) {
+        superScript.passiveAggresive=(int) Mathf.Floor(slide.value*100);
+        } else if (pointType==2) {
+        superScript.shyConfidence=(int) Mathf.Floor(slide.value*100);
+        } else if (pointType==3) {
+        superScript.nerdCool=(int) Mathf.Floor(slide.value*100);
+        } 
     }
 }
