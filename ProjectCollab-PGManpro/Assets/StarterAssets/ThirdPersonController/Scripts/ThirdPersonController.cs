740d628ad35e027031a6ed70c54fb71afc6f8fc9
Seryxss <patrick.alexander1121@gmail.com>
2023-10-30 23:49:41 +0700

    Ngubah input movement dkk

--- ProjectCollab-PGManpro/Assets/StarterAssets/ThirdPersonController/Scripts/ThirdPersonController.cs
+++ ProjectCollab-PGManpro/Assets/StarterAssets/ThirdPersonController/Scripts/ThirdPersonController.cs
@@ -28,20 +28,22 @@ namespace StarterAssets
         [Tooltip("Acceleration and deceleration")]
         public float SpeedChangeRate = 10.0f;
 
+        public float LookSensitivity = 1f;
+
         public AudioClip LandingAudioClip;
         public AudioClip[] FootstepAudioClips;
         [Range(0, 1)] public float FootstepAudioVolume = 0.5f;
 
         [Space(10)]
-        [Tooltip("The height the player can jump")]
-        public float JumpHeight = 1.2f;
+        // [Tooltip("The height the player can jump")]
+        // public float JumpHeight = 1.2f;
 
         [Tooltip("The character uses its own gravity value. The engine default is -9.81f")]
         public float Gravity = -15.0f;
 
         [Space(10)]
-        [Tooltip("Time required to pass before being able to jump again. Set to 0f to instantly jump again")]
-        public float JumpTimeout = 0.50f;
+        // [Tooltip("Time required to pass before being able to jump again. Set to 0f to instantly jump again")]
+        // public float JumpTimeout = 0.50f;
 
         [Tooltip("Time required to pass before entering the fall state. Useful for walking down stairs")]
         public float FallTimeout = 0.15f;
@@ -88,13 +90,13 @@ namespace StarterAssets
         private float _terminalVelocity = 53.0f;
 
         // timeout deltatime
-        private float _jumpTimeoutDelta;
-        private float _fallTimeoutDelta;
+        // private float _jumpTimeoutDelta;
+        // private float _fallTimeoutDelta;
 
         // animation IDs
         private int _animIDSpeed;
         private int _animIDGrounded;
-        private int _animIDJump;
+        // private int _animIDJump;
         private int _animIDFreeFall;
         private int _animIDMotionSpeed;
 
@@ -148,15 +150,15 @@ namespace StarterAssets
             AssignAnimationIDs();
 
             // reset our timeouts on start
-            _jumpTimeoutDelta = JumpTimeout;
-            _fallTimeoutDelta = FallTimeout;
+            // _jumpTimeoutDelta = JumpTimeout;
+            // _fallTimeoutDelta = FallTimeout;
         }
 
         private void Update()
         {
             _hasAnimator = TryGetComponent(out _animator);
 
-            JumpAndGravity();
+            // JumpAndGravity();
             GroundedCheck();
             Move();
         }
@@ -170,7 +172,7 @@ namespace StarterAssets
         {
             _animIDSpeed = Animator.StringToHash("Speed");
             _animIDGrounded = Animator.StringToHash("Grounded");
-            _animIDJump = Animator.StringToHash("Jump");
+            //_animIDJump = Animator.StringToHash("Jump");
             _animIDFreeFall = Animator.StringToHash("FreeFall");
             _animIDMotionSpeed = Animator.StringToHash("MotionSpeed");
         }
@@ -198,8 +200,8 @@ namespace StarterAssets
                 //Don't multiply mouse input by Time.deltaTime;
                 float deltaTimeMultiplier = IsCurrentDeviceMouse ? 1.0f : Time.deltaTime;
 
-                _cinemachineTargetYaw += _input.look.x * deltaTimeMultiplier;
-                _cinemachineTargetPitch += _input.look.y * deltaTimeMultiplier;
+                _cinemachineTargetYaw += _input.look.x * deltaTimeMultiplier * LookSensitivity;
+                _cinemachineTargetPitch += _input.look.y * deltaTimeMultiplier * LookSensitivity; 
             }
 
             // clamp our rotations so our values are limited 360 degrees
@@ -279,74 +281,74 @@ namespace StarterAssets
             }
         }
 
-        private void JumpAndGravity()
-        {
-            if (Grounded)
-            {
-                // reset the fall timeout timer
-                _fallTimeoutDelta = FallTimeout;
+        // private void JumpAndGravity()
+        // {
+        //     if (Grounded)
+        //     {
+        //         // reset the fall timeout timer
+        //         _fallTimeoutDelta = FallTimeout;
 
-                // update animator if using character
-                if (_hasAnimator)
-                {
-                    _animator.SetBool(_animIDJump, false);
-                    _animator.SetBool(_animIDFreeFall, false);
-                }
+        //         // update animator if using character
+        //         if (_hasAnimator)
+        //         {
+        //             _animator.SetBool(_animIDJump, false);
+        //             _animator.SetBool(_animIDFreeFall, false);
+        //         }
 
-                // stop our velocity dropping infinitely when grounded
-                if (_verticalVelocity < 0.0f)
-                {
-                    _verticalVelocity = -2f;
-                }
+        //         // stop our velocity dropping infinitely when grounded
+        //         if (_verticalVelocity < 0.0f)
+        //         {
+        //             _verticalVelocity = -2f;
+        //         }
 
-                // Jump
-                if (_input.jump && _jumpTimeoutDelta <= 0.0f)
-                {
-                    // the square root of H * -2 * G = how much velocity needed to reach desired height
-                    _verticalVelocity = Mathf.Sqrt(JumpHeight * -2f * Gravity);
+        //         // Jump
+        //         if (_input.jump && _jumpTimeoutDelta <= 0.0f)
+        //         {
+        //             // the square root of H * -2 * G = how much velocity needed to reach desired height
+        //             _verticalVelocity = Mathf.Sqrt(JumpHeight * -2f * Gravity);
 
-                    // update animator if using character
-                    if (_hasAnimator)
-                    {
-                        _animator.SetBool(_animIDJump, true);
-                    }
-                }
+        //             // update animator if using character
+        //             if (_hasAnimator)
+        //             {
+        //                 _animator.SetBool(_animIDJump, true);
+        //             }
+        //         }
 
-                // jump timeout
-                if (_jumpTimeoutDelta >= 0.0f)
-                {
-                    _jumpTimeoutDelta -= Time.deltaTime;
-                }
-            }
-            else
-            {
-                // reset the jump timeout timer
-                _jumpTimeoutDelta = JumpTimeout;
+        //         // jump timeout
+        //         if (_jumpTimeoutDelta >= 0.0f)
+        //         {
+        //             _jumpTimeoutDelta -= Time.deltaTime;
+        //         }
+        //     }
+        //     else
+        //     {
+        //         // reset the jump timeout timer
+        //         _jumpTimeoutDelta = JumpTimeout;
 
-                // fall timeout
-                if (_fallTimeoutDelta >= 0.0f)
-                {
-                    _fallTimeoutDelta -= Time.deltaTime;
-                }
-                else
-                {
-                    // update animator if using character
-                    if (_hasAnimator)
-                    {
-                        _animator.SetBool(_animIDFreeFall, true);
-                    }
-                }
+        //         // fall timeout
+        //         if (_fallTimeoutDelta >= 0.0f)
+        //         {
+        //             _fallTimeoutDelta -= Time.deltaTime;
+        //         }
+        //         else
+        //         {
+        //             // update animator if using character
+        //             if (_hasAnimator)
+        //             {
+        //                 _animator.SetBool(_animIDFreeFall, true);
+        //             }
+        //         }
 
-                // if we are not grounded, do not jump
-                _input.jump = false;
-            }
+        //         // if we are not grounded, do not jump
+        //         _input.jump = false;
+        //     }
 
-            // apply gravity over time if under terminal (multiply by delta time twice to linearly speed up over time)
-            if (_verticalVelocity < _terminalVelocity)
-            {
-                _verticalVelocity += Gravity * Time.deltaTime;
-            }
-        }
+        //     // apply gravity over time if under terminal (multiply by delta time twice to linearly speed up over time)
+        //     if (_verticalVelocity < _terminalVelocity)
+        //     {
+        //         _verticalVelocity += Gravity * Time.deltaTime;
+        //     }
+        // }
 
         private static float ClampAngle(float lfAngle, float lfMin, float lfMax)
         {
