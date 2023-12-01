d317495d135053e949ed31f116c30f6e7dc044d4
Patrick Alexander <118601385+Seryxss@users.noreply.github.com>
2023-12-01 21:14:50 +0700

    loading screen

--- ProjectCollab-PGManpro/Assets/Scripts/player/TeleportCollision.cs
+++ ProjectCollab-PGManpro/Assets/Scripts/player/TeleportCollision.cs
@@ -2,10 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class TeleportCollision : MonoBehaviour, InterfaceInteractable
 {
     public string SceneName;
+    [SerializeField] GameObject loadingScreen;
+    [SerializeField] GameObject interact;
+
+    //[SerializeField] private Slider loadingSlider;
 
     public string GetInteractText()
     {
@@ -21,9 +26,23 @@ public class TeleportCollision : MonoBehaviour, InterfaceInteractable
     {
         GameVariable gameVariable = FindObjectOfType<GameVariable>();  
         superScript.setVariable(gameVariable.score,gameVariable.stress, gameVariable.timeNow, gameVariable.day);
+        interact.SetActive(false);
+        loadingScreen.SetActive(true);
+        StartCoroutine(LoadLevelAsync(SceneName));
         SceneManager.LoadScene(SceneName);
     }
+    
+    IEnumerator LoadLevelAsync(string leveltoload)
+    {
+        AsyncOperation loadOperation = SceneManager.LoadSceneAsync(SceneName);
 
+        while (!loadOperation.isDone)
+        {
+            float progressValue = Mathf.Clamp01(loadOperation.progress / 0.9f);
+            //loadingSlider.value = progressValue;
+            yield return null;
+        }
+    }
     public void Interact(Transform interactorTransform)
     {
         ToggleDoor();
