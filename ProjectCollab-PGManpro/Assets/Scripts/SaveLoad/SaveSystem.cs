b6971ab64574e2d0cc8e4736e1bbd9936ee3a221
louisarthurw <c14210048@john.petra.ac.id>
2023-12-10 22:47:16 +0700

    script save load blm sempurna, hapus tombol load di pause

--- ProjectCollab-PGManpro/Assets/Scripts/SaveLoad/SaveSystem.cs
+++ ProjectCollab-PGManpro/Assets/Scripts/SaveLoad/SaveSystem.cs
@@ -1,38 +1,59 @@
-// using System.IO;
-// using UnityEngine;
-// using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.Serialization.Formatters.Binary;
 
-// public class SaveSystem 
-// {
-//     public static void SavePlayer (Player player)
-//     {
-//         BinaryFormatter formatter = new BinaryFormatter();
-//         string path = Application.persistentDataPath + "/player.fun";
-//         FileStream stream = new FileStream(path, FileMode.Create);
+public class SaveSystem 
+{
+    public void SaveGame(GameVariable gameVariable, Vector3 playerPosition)
+    {
+        BinaryFormatter formatter = new BinaryFormatter();
+        string path = Application.persistentDataPath + "/gameData.fun";
+        FileStream stream = new FileStream(path, FileMode.Create);
+        PlayerData data = new PlayerData(gameVariable, playerPosition);
 
-//         PlayerData data = new PlayerData(player);
+        // FindObjectOfType<GameManager>().updateSuperScript();
+        data.boy = superScript.boy;
+        data.username = superScript.username;
+        data.passiveAggresive = superScript.passiveAggresive;
+        data.shyConfidence = superScript.shyConfidence;
+        data.nerdCool = superScript.nerdCool;
+        data.indexDialog = superScript.indexDialog;
+        data.tasks = superScript.Tasks;
+        data.items = superScript.itemOnwed;
 
-//         formatter.Serialize(stream, data);
-//         stream.Close();
-//     }
+        formatter.Serialize(stream, data);
+        stream.Close();
+    }
 
-//     public static PlayerData LoadPlayer()
-//     {
-//         string path = Application.persistentDataPath + "/player.fun";
+    public PlayerData LoadGame()
+    {
+        string path = Application.persistentDataPath + "/gameData.fun";
 
-//         if(File.Exists(path))
-//         {
-//             BinaryFormatter formatter = new BinaryFormatter();
-//             FileStream stream = new FileStream(path, FileMode.Open);
+        if(File.Exists(path))
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            FileStream stream = new FileStream(path, FileMode.Open);
 
-//             PlayerData data = formatter.Deserialize(stream) as PlayerData;
-//             stream.Close();
+            PlayerData data = formatter.Deserialize(stream) as PlayerData;
+            stream.Close();
 
-//             return data;
-//         } else
-//         {
-//             Debug.LogError("Save file not found in " + path);
-//             return null;
-//         }
-//     }
-// }
+            superScript.boy = data.boy;
+            superScript.username = data.username;
+            superScript.passiveAggresive = data.passiveAggresive;
+            superScript.shyConfidence = data.shyConfidence;
+            superScript.nerdCool = data.nerdCool;
+            superScript.indexDialog = data.indexDialog;
+            superScript.Tasks = data.tasks;
+            superScript.itemOnwed = data.items;
+            
+            return data;
+        } 
+        else
+        {
+            Debug.LogError("Save file not found in " + path);
+            return null;
+        }
+    }
+}
